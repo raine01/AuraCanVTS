9a81bba9e820c26c7ef17e239671697f84eb51e7
raine01 <1047901426@qq.com>
2024-12-08 00:11:47 +0800

    修了三四十个bug,勉强不报错了

--- vts-5.cs
+++ vts-5.cs
@@ -42,12 +42,10 @@ public static class AuraCanVTS {
 			switch (commandType) {
 				case CommandTypeEnum.createparameter://创建新参数
 				case CommandTypeEnum.deleteparameter://删除参数
-				case CommandTypeEnum.executecommandset://设置参数值
-				case CommandTypeEnum.executecommandpress://触发热键
+				case CommandTypeEnum.executecommand://执行指令
 					AuraCanSocket.SendToVTubeStudio(new Message(parameters).Serialize());
 					break;
-				case CommandTypeEnum.createcommandset://创建指令(设置参数)
-				case CommandTypeEnum.createcommandpress://创建指令(热键)
+				case CommandTypeEnum.createcommand://创建指令
 					VtsCreateCommand(shortCommandStr, parameters);
 					break;
 				case CommandTypeEnum.deletecommand://删除指令
@@ -77,7 +75,7 @@ public static class AuraCanVTS {
 
 	public static void Init() {
 		AuraCanSocket.Init();
-		Logger.SetLogger(0);// TODO 先直接传0,后续读持久变量
+		Logger.SetLogger(-3);// TODO 先直接传0,后续读持久变量
 	}
 
 	private static CommandTypeEnum CheckCommand(string commandStr, out string longCommandStr, out string shortCommandStr, out Dictionary<string, object> parameters)
@@ -168,11 +166,11 @@ public static class AuraCanVTS {
 			}
 			string longCmdStr = (string)strAndPar["longCmdStr"];
 			string shortCmdStr = (string)strAndPar["shortCmdStr"];
-			String payload = new Message((string)strAndPar["parameters"]).Serialize();
+			String payload = new Message((Dictionary<string, object>)strAndPar["parameters"]).Serialize();
 
 			//校验条件(顺便创建executer和judges)
 			string condition = match.Groups[3].Value;//传入参数
-			if(!CheckCondition(condition, payload, out prepareConditionStr, out Judge[] judges))
+			if(!CheckCondition(condition, payload, out string prepareConditionStr, out Judge[] judges))
 			{
 				throw new Exception($"{pluginName} {prepareConditionStr}。");
 			}
@@ -253,8 +251,8 @@ public static class AuraCanVTS {
 	{
 		//创建command对象
 		Judge[] judges = (Judge[])parameters["judges"];
-		parameters.RemoveKey("judges");
-		Command command = new Command(parameters);
+		parameters.Remove("judges");
+		Command command = new Command(judges);
 		//获取序号
 		int newCommandKey = 0;
 		int[] commandKeys = _commands.Keys.ToArray();
@@ -273,14 +271,15 @@ public static class AuraCanVTS {
 		//Log($"{_handles.ToString()} {_commands.ToString()} {_commandDic.ToString()}");
 	}
 
-	private static void VtsDeleteCommand(int commandIndex)
+	private static void VtsDeleteCommand(Dictionary<string, object> parameters)
 	{
+		int commandIndex = (int)parameters["commandIndex"];
 		//删_commands中的command
-		if(_commands.ContainsKey(commandIndex))
+		/*if(_commands.ContainsKey(commandIndex))
 		{
 			_commands[commandIndex].Destroy();//不再让judge指向command
 			_commands.Remove(commandIndex);
-		}
+		}*/
 		//删_handles中的Judge(从handle里面删judge的时候注意下，可能要删不止一次)
 		//通过commandIndex从_commands中拿到Command
 		//遍历Command中的judge数组,从数组的judge中拿到judgeKey(_key)
@@ -288,14 +287,14 @@ public static class AuraCanVTS {
 		//根据handleNo拿到Handle
 		//调用Handle的RemoveJudge方法,传入judgeKey,judge
 		
-		RemoveJudge(string judgeKey, Judge judge)
+		//RemoveJudge(string judgeKey, Judge judge);
 		//删持久变量
-		VariableDictionary dv = StaticHelpers.GetDictVariable(true, pluginName);
+		/*VariableDictionary dv = StaticHelpers.GetDictVariable(true, pluginName);
 		if(dv != null)
 		{
 			Log($"delete {commandIndex.ToString()} {pluginName}");
 			dv.RemoveKey(commandIndex.ToString(), pluginName);
-		}
+		}*/
 	}
 
 	private static void VtsShowCommand()
@@ -307,7 +306,7 @@ public static class AuraCanVTS {
 			dv = StaticHelpers.GetDictVariable(true, pluginName);
 		}
 		string allCommands = String.Join("\n", dv.Values.Select(pair => $"{pair.Key}{pair.Value}"));
-		Log(allCommands);
+		Logger.Log(allCommands);
 	}
 
 	//true表示没问题,out格式化后的;false表示有问题,out异常提示
@@ -315,6 +314,7 @@ public static class AuraCanVTS {
 	{
 		// todo 得检查下有没有这个参数
 		// todo 还得查查各式是否合理
+		prepareConditionStr = conditionStr;// todo 先直接放上了
 		string[] parts = Regex.Split(conditionStr, @"\s+and\s+");
 		int partsLength = parts.Length;
 		//创建executer
@@ -349,7 +349,14 @@ public static class AuraCanVTS {
 				val = double.Parse(match.Groups[4].Value);
 			}
 			Judge judge = new Judge(executer, i, judgeKey, method, val);
-			_handles
+			foreach(string handleNo in AuraCanDictionary.GetHandleNosByJudgeKey(judgeKey))
+			{
+				if(!_handles.ContainsKey(handleNo))
+				{
+					_handles[handleNo] = new Handle(handleNo);
+				}
+				_handles[handleNo].AddJudge(name, judge);
+			}
 			judges[i] = judge;
 		}
 		return true;
@@ -410,6 +417,215 @@ public static class AuraCanVTS {
 	}
 }
 
+public static class AuraCanDictionary {
+	private static Dictionary<string, string> _commandDic = new Dictionary<string, string>//日志字段字典
+	{
+		{ "hp", "24,1,5,6;39,1,2,3" },//handleNo,name,judgeKey,judgeKey.ToUpper()
+		{ "area", "40,,2" },
+		{ "areasub", "40,,3" }
+	};
+	public static string[] GetHandleNosByJudgeKey(string judgeKey)
+	{
+		List<string> noList = new List<string>();
+		string commandDicValue = _commandDic[judgeKey];
+		foreach(string handlePart in commandDicValue.Split(';'))
+		{
+			string handleNo = handlePart.Split(',')[0];
+			noList.Add(handleNo);
+		}
+		return noList.ToArray();
+	}
+	public static int GetNameIndexByHandleNo(string handleNo)
+	{
+		foreach(string commandInfo in _commandDic.Values)
+		{
+			string[] handles = commandInfo.Split(';');
+			foreach(string handle in handles)
+			{
+				string[] pars = handle.Split(',');
+				if(handleNo == pars[0])
+				{
+					return pars[1] == "" ? 0 : int.Parse(pars[1]);
+				}
+			}
+		}
+		return 0;//0表示日志无实体字段
+	}
+	public static Dictionary<string, int> GetParsIndexByHandleNo(string handleNo)
+	{
+		Dictionary<string, int> parsIndex = new Dictionary<string, int>();
+		foreach(KeyValuePair<string, string> kvp in _commandDic)
+		{
+			foreach(string handlePart in kvp.Value.Split(';'))
+			{
+				string[] logInfo = handlePart.Split(',');
+				if(logInfo[0] == handleNo)
+				{
+					parsIndex.Add(kvp.Key, int.Parse(logInfo[2]));
+					if(logInfo.Length > 3)
+					{
+						parsIndex.Add(kvp.Key.ToUpper(), int.Parse(logInfo[3]));
+					}
+					continue;
+				}
+			}
+		}
+		return parsIndex;
+	}
+}
+
+public class Command
+{
+	private Judge[] _judges;
+	public Command(Judge[] judges)
+	{
+		_judges = judges;
+	}
+}
+
+public class Handle
+{
+	private Dictionary<string, LinkedList<Judge>> _judges; //不同实体的判断列表
+	private Dictionary<string, int> _parsIndex; //参数,位于日志中第几个
+	private int _nameIndex;//日志中第几个字段是实体的名字
+	public Handle(string handleNo)
+	{
+		_judges = new Dictionary<string, LinkedList<Judge>>();
+		_parsIndex = AuraCanDictionary.GetParsIndexByHandleNo(handleNo);
+		_nameIndex = AuraCanDictionary.GetNameIndexByHandleNo(handleNo);
+	}
+	public void AddJudge(string name, Judge judge)
+	{
+		if(_judges.ContainsKey(name))
+		{
+			_judges[name].AddLast(judge);
+		}
+		else
+		{
+			LinkedList<Judge> judgeList = new LinkedList<Judge>();
+			judgeList.AddLast(judge);
+			_judges.Add(name, judgeList);
+		}
+	}
+	public void Process(string[] log)
+	{
+		Dictionary<string, string> pars = new Dictionary<string, string>();
+		foreach(KeyValuePair<string, int> kvp in _parsIndex)
+		{
+			pars.Add(kvp.Key,log[kvp.Value]);
+			//Logger.Log($"parsAdd {kvp.Key} {log[kvp.Value]}");
+		}
+		if(_nameIndex != 0 && _judges.ContainsKey(log[_nameIndex]))//特定实体的判断列表
+		{
+			foreach (Judge judge in _judges[log[_nameIndex]])
+			{
+				Logger.Log($"judge {log[_nameIndex]} ");
+				judge.Process(pars);
+			}
+		}
+		if(_judges.ContainsKey(""))//任意实体的判断列表
+		{
+			foreach (Judge judge in _judges[""])
+			{
+				Logger.Log($"judge all ");
+				judge.Process(pars);
+			}
+		}
+	}
+}
+
+public class Judge
+{
+	private Executer _executer;
+	private int _index;//Executer数组中的索引
+	private string _key;//比较参数在handle方法参数中的key
+	private JudgeMethodEnum _method;
+	private double _numVal;
+	private string _strVal;
+	public Judge(Executer executer, int index, string judgeKey, JudgeMethodEnum method, object val)
+	{
+		_executer = executer;
+		_index = index;
+		_key = judgeKey;
+		_method = method;
+		if (val is int || val is double)
+		{
+			_numVal = (double) val;
+		}
+		else
+		{
+			_strVal = (string) val;
+		}
+	}
+	public void Process(Dictionary<string, string> pars)
+	{
+		double percent;
+		switch(_method)
+		{
+			case JudgeMethodEnum.gt:
+				_executer.CheckSaveAndSend(_index, double.Parse(pars[_key]) > _numVal);
+				break;
+			case JudgeMethodEnum.lt:
+				_executer.CheckSaveAndSend(_index, double.Parse(pars[_key]) < _numVal);
+				break;
+			case JudgeMethodEnum.eq:
+				_executer.CheckSaveAndSend(_index, pars[_key] == _strVal);
+				break;
+			case JudgeMethodEnum.ne:
+				_executer.CheckSaveAndSend(_index, pars[_key] != _strVal);
+				break;
+			case JudgeMethodEnum.gtp://>%
+				percent = double.Parse(pars[_key]) * 100 / double.Parse(pars[_key.ToUpper()]);
+				_executer.CheckSaveAndSend(_index, percent > _numVal);
+				break;
+			case JudgeMethodEnum.ltp://<%
+				percent = double.Parse(pars[_key]) * 100 / double.Parse(pars[_key.ToUpper()]);
+				_executer.CheckSaveAndSend(_index, percent < _numVal);
+				break;
+			default:
+				throw new Exception($"{pluginName} 钝口螈!有活力的钝口螈!");
+				break;
+		}
+	}
+}
+
+public class Executer
+{
+	private bool[] _flags;
+	private string _payload;
+	public Executer(int length, string payload)
+	{
+		_flags = new bool[length];
+		_payload = payload;
+	}
+	public void CheckSaveAndSend(int index, bool checkResult)
+	{
+		bool before = _flags.All(x => x);
+		_flags[index] = checkResult;
+		bool after = _flags.All(x => x);
+		Logger.Log($"before {before} after {after}, checkResult {checkResult}");
+		if(!before&&after)
+		{
+			AuraCanSocket.SendToVTubeStudio(_payload);
+		}
+	}
+}
+
+public class Message {
+	public string apiName { get; set; } = "VTubeStudioPublicAPI";
+	public string apiVersion { get; set; } = "1.0";
+	public string messageType { get; set; }
+	public Dictionary<string, object> data { get; set; }
+
+	public Message(Dictionary<string, object> data) {
+		this.messageType = data["type"].ToString();
+		data.Remove("type");
+		this.data = data;
+	}
+	public string Serialize() => StaticHelpers.Serialize(this, false);
+}
+
+
 public static class AuraCanSocket {
 	static WebSocket ws;
 	public static void SendToVTubeStudio(string payload)
@@ -442,18 +658,19 @@ public static class AuraCanSocket {
 	}
 	private static void Authenticate() {
 		string token = StaticHelpers.GetScalarVariable(true, "ACtoken") ?? "";
-		MessageTypeEnum commandType;
+		MessageTypeEnum type;
 		Dictionary<string, object> parameters = new Dictionary<string, object>();
 		if (token == "") {
-			commandType = MessageTypeEnum.AuthenticationTokenRequest;
+			type = MessageTypeEnum.AuthenticationTokenRequest;
 			parameters["pluginIcon"] = "/9j/4AAQSkZJRgABAgEARwBHAAD//gAmUGFpbnQgVG9vbCAtU0FJLSBKUEVHIEVuY29kZXIgdjEuMDAA/9sAhAAzJiYmJiY1NTU1SUlJSUliYmJiYmJ7e3t7e3t7mZmZmZmZmZm8vLy8vLy839/f39/f////////////////////AU46Ojo6OlNTU1NxcXFxcZSUlJSUlLy8vLy8vLzp6enp6enp6f//////////////////////////////////////wAARCACAAIADAREAAhEBAxEB/8QBogAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoLEAACAQMDAgQDBQUEBAAAAX0BAgMABBEFEiExQQYTUWEHInEUMoGRoQgjQrHBFVLR8CQzYnKCCQoWFxgZGiUmJygpKjQ1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4eLj5OXm5+jp6vHy8/T19vf4+foBAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKCxEAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwCgeaBCgUgHUAGOKAGmgBMZpjH7ccYoAdHbvKeBxQBP5EUZww3GgB48gf8ALIUwsxnkxSNjbj6UAVZI2jJBFICOgAoAUcUXEJ3pgSqF3jPSkAsm3dx0pANBpgO3fLil1AYaqwAvBoAsW8XnSY7d6QzQwqLgCgCsVLNQA6SMAAihgmR9KBizBZEyeooEUGG04oAAMimloIbSGA60IB/FMQlKwDgtNIBSMUWAbimAd6QGlZoFiz6mk9ylsSSGkALgcVRISfcP1oY0V+1IY0jigCtMvehCEjGVNUthMjI5pDEHWhAPA4piFVc0noA8DAprYQ1qEMD0pgIOtSBp27DykHtSe5S2BjzSAYrjeBmmIdI3IFDBEJ60hkiKCpBNNCY14P3bjrmnYVyrCPlNNbAyIjBNIBvekBNjin1EA4FDVxiBqa2AAM800IGGKTGAHFK+oFuAFkwpwQaT3GtiQ5ZwM9BQMa0SoQR2oAGOTmgBhIpAN3jpimA9XdaLisDKHBZRz3FNMTRRahgNpAWGGDVdQGkcUPYCM0kBMo+WqENbrS3GANT1As2jYcihjRLtZZMnpQMSQ5FADOwpAMegBqUwJAxWgGRtKUenYVwd4JfvcH1oEQmKPtJ+lIZNtOMmnfUVtBjDiqewEWOakB5b5cUwEzmkA3dSAntiTKtFtBrctFQ52kn2pDI3IAxTAO1IBrjBFMBo+9SAnjAGaaEyvdLtemIrGgA6UAXxjy6z15iuhC61q3oTYiC80IAK80AN2nmlIAxmhICe2GJF+tN7AtyzPleR1qCisPmb6U+gtbkoxQMqSzM7nB4oENRijBqANGMhsEdCKYMZcpuj+lAijimA/bxRYRbA+SsnuWMKEkGnzaBYQpjmqixNAAChNDb5gS0GA7RRLVggVeKd9QtoPj++v1FN7CW5NcHPFQURcLQBBLKW4HSmIiAoAXApiLFrPtYI3TtQBcYAigCg6bHIoAeBxTuKxc2gJWRoRE4FAEancDmqWgrEYbnFOQkI7jPFJajJYh8poYCoh3Zp9BA7gHrzSGQO5PApoQzbigBpxTASgQ2kM1om3oD6imIhuI8/MKAREFOKlsZb3ikUJtVsmgCuCBvpiIMmmITihAyzDkihrUE9CUYIINLqO2hWYZPFMBpAUUCIiSaYDaBC0ANNIZoWRPlYPY0xMsMAcigCIRjNTJFLUYDxQwFY7YutAFb8aADC+tADNpJpoRKHK8CmA4yfLtFK2o76DQaBDHOefSgCI0AJTAKBCHrSGaNr/q6aEyc0ARscc0pLQcXZkG5RRZhdCMyMME0WYXQz91RZhdB+6oswuhQ0Qosx3QuUI4oSsF0xuVWgQ0uA6mgAb7goAiNACUwCgQh60hlqK42CmIkN03BFFwsOSdZOMYoAqbjQAmaAFG2noAuFo0AXC0aAP4xUjI2b5m+mKAIc0ASxncpHpQAxuDigBtABQAnegB2aYhwbigYK208UCDaaAExQAuKAEoAMnFAEyNnikMjbjNAEZoAdCSHx6igBD1oASgApgIetIB1OwgosAlAE22qsIaykUWGMyaQBQAoBNFgHopBFDQwfjNSBE1ABGcOtADj1oAb3oAKAEPWgB5QirsK4bTSsAlAH/9k=";
 		} else {
-			commandType = MessageTypeEnum.AuthenticationRequest;
+			type = MessageTypeEnum.AuthenticationRequest;
 			parameters["authenticationToken"] = token;
 		}
 		parameters["pluginName"] = pluginName;
 		parameters["pluginDeveloper"] = developer;
-		string payload = new Message(commandType, parameters).Serialize();
+		parameters["type"] = type;
+		string payload = new Message(parameters).Serialize();
 		SendToVTubeStudio(payload);
 	}
 	private static void ProcessReceivedMessage(string json) {
@@ -485,98 +702,11 @@ public static class AuraCanSocket {
 	}
 }
 
-public static class AuraCanDictionary {
-	private static Dictionary<string, string> _commandDic = new Dictionary<string, string>//日志字段字典
-	{
-		{ "hp", "24,1,5,6;39,1,2,3" },//24行字段5和39行字段2是需要比较的值(1是主体,6和3是需要作为除数的值)
-		{ "area", "40,,2" },//40行字段3(日志无实体)
-		{ "areaSub", "40,,3" }
-	};
-
-
-}
-
-public class Command
-{
-	private Judge[] _judges;
-	public Command(, string commandStr, Dictionary<string, object> parameters)
-	{
-		//创建
-		MessageTypeEnum type = (MessageTypeEnum)parameters["type"]；
-		parameters.RemoveKey("type");
-		Judge[] judges = parameters["judges"];
-		for(int i=0; i<judges.Length; i++)
-		{
-			AuraCanVTS.AddJudgeToHandles(name, judgeKey, judge);
-		}
-		flags = new bool[partsLength];
-		_payload = new Message(type, parameters).Serialize();
-	}
-}
-
-public class Handle
-{
-	private Dictionary<string, LinkedList<Judge>> _judges; //不同实体的判断列表
-	private Dictionary<string, int> _parsIndex; //参数,这个参数位于日志中第几个
-	private Dictionary<string, int> _parsCount; //参数,这个参数使用的次数
-	private int _nameIndex;//日志中第几个字段是实体的名字
-}
-
-public class Judge
-{
-	private Executer _executer;
-	private int _index;//Executer数组中的索引
-	private string _key;//比较参数在handle方法参数中的key
-	private JudgeMethodEnum _method;
-	private double _numVal;
-	private string _strVal;
-	public Judge(Executer executer, int index, string judgeKey, JudgeMethodEnum method, object val)
-	{
-		_executer = executer;
-		_index = index;
-		_key = judgeKey;
-		_method = method;
-		if (val is int || val is double)
-		{
-			_numVal = (double) val;
-		}
-		else
-		{
-			_strVal = (string) val;
-		}
-	}
-}
-
-public class Executer
-{
-	private bool[] _flags;
-	private string _payload;
-	public Executer(int length, string payload)
-	{
-		_flags = new bool[length];
-		_payload = payload;
-	}
-}
-
-public class Message {
-	public string apiName { get; set; } = "VTubeStudioPublicAPI";
-	public string apiVersion { get; set; } = "1.0";
-	public string messageType { get; set; }
-	public Dictionary<string, object> data { get; set; }
-
-	public Message(Dictionary<string, object> data) {
-		this.messageType = data["type"].ToString();
-		data.RemoveKey("type");
-		this.data = data;
-	}
-	public string Serialize() => StaticHelpers.Serialize(this, false);
-}
-
 public enum CommandTypeEnum
 {
 	createparameter, //创建参数
 	deleteparameter, //删除参数
-	createcommand//创建命令
+	createcommand,//创建命令
 	deletecommand, //删除命令
 	showcommand, //列出已创建的命令
 	executecommand, //立刻执行
